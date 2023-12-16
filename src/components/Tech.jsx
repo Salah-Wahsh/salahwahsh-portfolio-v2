e5d432ccf399fdedd23d306dabdd67db06c5b133
“salahwahsh12” <salahwahsh12@gmail.com>
2023-12-16 12:42:38 +0200

    - tech section reduce balls for mobile view

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -4,14 +4,23 @@ import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 
 const Tech = () => {
+  const isMobileView = window.innerWidth <= 768;
+
   return (
     <div className="flex flex-row flex-wrap justify-center gap-12">
-      {technologies.map((technology) => (
-        <div className="w-28 h-28 text-center" key={technology.name}>
-          <BallCanvas icon={technology.icon} />
-          <h3 className="mt-1">{technology.name}</h3>
-        </div>
-      ))}
+      {isMobileView
+        ? technologies.slice(3, 13).map((technology) => (
+            <div className="w-28 h-28 text-center" key={technology.name}>
+              <BallCanvas icon={technology.icon} />
+              <h3 className="mt-1">{technology.name}</h3>
+            </div>
+          ))
+        : technologies.map((technology) => (
+            <div className="w-28 h-28 text-center" key={technology.name}>
+              <BallCanvas icon={technology.icon} />
+              <h3 className="mt-1">{technology.name}</h3>
+            </div>
+          ))}
     </div>
   );
 };
