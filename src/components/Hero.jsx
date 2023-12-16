8eda27861e39785de53c91973920e0b8a2b495ac
“salahwahsh12” <salahwahsh12@gmail.com>
2023-12-16 11:51:46 +0200

    - add static image for mobile view

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,10 @@
 import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
+import mobileImage from "../assets/laptop.png";
 
 const Hero = () => {
+  const isMobileView = window.innerWidth <= 768;
   return (
     <section className="relative w-full h-screen bg-hero-pattern bg-cover mx-auto ">
       Hero
@@ -22,7 +24,14 @@ const Hero = () => {
           </p>
         </div>
       </div>
-      <ComputersCanvas />
+      {isMobileView ? (
+        <img
+          src={mobileImage}
+          className="absolute bottom-0  w-[100%] h-[100%] object-contain"
+        />
+      ) : (
+        <ComputersCanvas />
+      )}
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
